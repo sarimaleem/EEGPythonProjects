9aaf6bb6082ff3770dca4d39fd844999040ec38c
sarimaleem <34404894+sarimaleem@users.noreply.github.com>
2018-12-02 17:29:22 -0600

    Add files via upload

---
+++ importFileTestPython.py
@@ -0,0 +1,27 @@
+import os
+import scipy.io as sio
+import sys
+
+directory = os.fsencode('positive_segments')
+os.chdir(r'positive_segments')
+os.chdir(r'/Users/admin/.spyder-py3')
+
+
+
+
+
+'''
+directory = os.fsencode('positive_segments')
+
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    sig = sio.loadmat(filename)
+'''  
+
+'''
+import sys
+sys.path.insert(0, '/path/to/application/app/folder')
+
+import file
+c282_02148.mat
+'''
